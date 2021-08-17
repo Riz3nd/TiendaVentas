3f5bb310dcf1604927190be3eff841357813780d
Riz3nd <88601633+Riz3nd@users.noreply.github.com>
2021-08-17 12:37:57 -0500

    Update Orden.java

--- TiendaVentas/src/co/com/tiendaventas/Orden.java
+++ TiendaVentas/src/co/com/tiendaventas/Orden.java
@@ -3,15 +3,19 @@ package co.com.tiendaventas;
 public class Orden {
 	
 	private int idOrden;	
-	Producto producto[];
-	private final int MAX_PRODUCTOS = 10;
+	private Producto productos[];
+	private static int contadorOrdenes;
+	private int contadorProductos;
+	private static final int MAX_PRODUCTOS = 10;
 	
-	
+	public Orden(){
+		this.idOrden = ++Orden.contadorOrdenes;
+		this.productos = new Producto[Orden.MAX_PRODUCTOS];
+	}
 	
 	
 	public void agregarProducto(Producto producto[]) {
 		
-				
 	}
 	
 	public Double calcularTotal(Double total){		
@@ -22,10 +26,6 @@ public class Orden {
 	
 	public void mostrarOrden(){
 		
-	}
-	
-	
-	
-	
+	}	
 	
 }
